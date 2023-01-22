cecf8c01463cc3b8b0fcb3932800e098896227f2
Natasha Okparavero <tasha.okparavero@yahoo.com>
2023-01-22 17:28:19 +0000

    Declared variables

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,28 @@
+// Declaring variable to be used later 
 
+let hourNow = moment().format('h');
+let hourNow24= parseInt(moment().format('HH'));
+let hourNowInt = parseInt(hourNow);
 
+const firstHour = $('#hour1').text();
+const secondHour = $('#hour2').text();
+const thirdHour = $('#hour3').text();
+const fourthHour = $('#hour4').text();
+const fithhHour = $('#hour5').text();
+const sixthHour = $('#hour6').text();
+const seventhHour = $('#hour7').text();
+const eighthHour = $('#hour8').text();
+const ninthHour = $('#hour9').text();
+
+const firstHourInt = parseInt(firstHour);
+const secondHourInt = parseInt(secondHour);
+const thirdHourInt = parseInt(thirdHour);
+const fourthHourInt = parseInt(fourthHour);
+const fithHourInt = parseInt(fithhHour);
+const sixthHourInt = parseInt(sixthHour);
+const seventhHourInt = parseInt(seventhHour);
+const eigthHourInt = parseInt(eighthHour);
+const ninthHourInt = parseInt(ninthHour);
 
 // I want to display the current day and time at the top of the calender when a user opens the planner
 
