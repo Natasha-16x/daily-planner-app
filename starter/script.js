56239751a733563f2299cb1cb18c02feda08d5f3
Natasha Okparavero <tasha.okparavero@yahoo.com>
2023-01-22 17:41:27 +0000

    added code for local storage - JSON

--- starter/script.js
+++ starter/script.js
@@ -8,7 +8,7 @@ const firstHour = $('#hour1').text();
 const secondHour = $('#hour2').text();
 const thirdHour = $('#hour3').text();
 const fourthHour = $('#hour4').text();
-const fithhHour = $('#hour5').text();
+const fifthHour = $('#hour5').text();
 const sixthHour = $('#hour6').text();
 const seventhHour = $('#hour7').text();
 const eighthHour = $('#hour8').text();
@@ -18,11 +18,18 @@ const firstHourInt = parseInt(firstHour);
 const secondHourInt = parseInt(secondHour);
 const thirdHourInt = parseInt(thirdHour);
 const fourthHourInt = parseInt(fourthHour);
-const fithHourInt = parseInt(fithhHour);
+const fifthHourInt = parseInt(fifthHour);
 const sixthHourInt = parseInt(sixthHour);
 const seventhHourInt = parseInt(seventhHour);
 const eigthHourInt = parseInt(eighthHour);
 const ninthHourInt = parseInt(ninthHour);
+
+// check to see if hours log 
+console.log(firstHour);
+
+// check for parseInt numbers 
+
+console.log(firstHourInt);
 
 // I want to display the current day and time at the top of the calender when a user opens the planner
 
@@ -47,11 +54,100 @@ displayTime();
   //Allow a user to enter an event when they click a timeblock
 
    // Save the event in local storage when the save button is clicked in that timeblock.
+// Store user input in local Storage
 
+$('#btn1').click(function() {
+    if($('#text1').val()) {
+      localStorage.removeItem('text1');
+    }
+  
+    const firstHourString = JSON.stringify(firstHour);
+    
+    localStorage.setItem(hour1, $('#text1').val());
+  });
+  
+  $('#btn2').click(function() {
+    if($('#text2').val()) {
+      localStorage.removeItem('text2');
+    }
+  
+    const secondHourString = JSON.stringify(secondHour);
+  
+    localStorage.setItem(hour2, $('#text2').val());
+  });
+  
+  $('#btn3').click(function() {
+    if($('#text3').val()) {
+      localStorage.removeItem('text3');
+    }
+  
+    const thirdHourString = JSON.stringify(thirdHour);
+  
+    localStorage.setItem(hour3, $('#text3').val());
+  });
+  
+  $('#btn4').click(function() {
+    if($('#text4').val()) {
+      localStorage.removeItem('text4');
+    }
+  
+    const fourthHourString = JSON.stringify(fourthHour);
+  
+    localStorage.setItem(hour4, $('#text4').val());
+  });
+  
+  $('#btn5').click(function() {
+    if($('#text5').val()) {
+      localStorage.removeItem('text5');
+    }
+  
+    const fifthHourString = JSON.stringify(fifthHour);
+  
+    localStorage.setItem(hour5, $('#text5').val());
+  });
+  
+  $('#btn6').click(function() {
+    if($('#text6').val()) {
+      localStorage.removeItem('text6');
+    }
+  
+    const sixthHourString = JSON.stringify(sixthHour);
+  
+    localStorage.setItem(hour6, $('#text6').val());
+  });
+  
+  $('#btn7').click(function() {
+    if($('#text7').val()) {
+      localStorage.removeItem('text7');
+    }
+  
+    const seventhHourString = JSON.stringify(seventhHour);
+  
+    localStorage.setItem(hour7, $('#text7').val());
+  })
+  
+  $('#btn8').click(function() {
+    if($('#text8').val()) {
+      localStorage.removeItem('text8');
+    }
+  
+    const eighthHourString = JSON.stringify(eighthHour);
+  
+    localStorage.setItem("text8", $('#text8').val());
+  })
+  
+  $('#btn9').click(function() { 
+    if($('#text9').val()) {
+      localStorage.removeItem('text9');
+    }
+  
+    const ninthHourString = JSON.stringify(ninthHour);
+  
+    localStorage.setItem(hour9, $('#text9').val());
+  })
+  
    
 
-
-   
 
 
    // Persist events between refreshes of a page
